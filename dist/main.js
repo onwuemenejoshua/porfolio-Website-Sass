ef3e3e817ac24f7e334e642877e0b21a986fb780
Onwuemene Joshua <onwuemenejoshua@gmail.com>
2022-11-06 22:12:56 +0100

    updated

--- dist/main.js
+++ dist/main.js
@@ -1,35 +1,21 @@
 const menuBtn = document.querySelector(".menu-btn");
 const openmenu = document.querySelector(".openMenu");
 const closemenu = document.querySelector(".closeMenu");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
 
 openmenu.addEventListener("click", show);
 closemenu.addEventListener("click", close);
 
 function show() {
-  //   mainMenu.style.display = "flex";
-  //   mainMenu.style.right = "0";
+  menuNav.style.display = "flex";
+  menuNav.style.right = "0";
   openmenu.style.display = "none";
   closemenu.style.display = "block";
 }
 
 function close() {
-  //   mainMenu.style.right = "-100%";
+  menuNav.style.right = "-100%";
   openmenu.style.display = "block";
   closemenu.style.display = "none";
 }
-
-// let showMenu = false;
-
-// menuBtn.addEventListener("click", toggleMenu);
-
-// function toggleMenu() {
-//   if (!showMenu) {
-//     hamburger.classList.add("open");
-
-//     showMenu = true;
-//   } else {
-//     hamburger.classList.remove(open);
-
-//     showMenu = false;
-//   }
-// }
