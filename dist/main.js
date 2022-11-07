16e3f77a769cd2e2e151321b3c9007989efdceca
Onwuemene Joshua <onwuemenejoshua@gmail.com>
2022-11-07 23:32:54 +0100

    correction made to the hamburger menu

--- dist/main.js
+++ dist/main.js
@@ -2,20 +2,20 @@ const menuBtn = document.querySelector(".menu-btn");
 const openmenu = document.querySelector(".openMenu");
 const closemenu = document.querySelector(".closeMenu");
 const nav = document.querySelector(".nav");
-const menuNav = document.querySelector(".menu-nav");
+// const menuNav = document.querySelector(".menu-nav");
 
 openmenu.addEventListener("click", show);
 closemenu.addEventListener("click", close);
 
 function show() {
-  menuNav.style.display = "flex";
-  menuNav.style.right = "0";
+  nav.style.display = "flex";
+  nav.style.right = "0";
   openmenu.style.display = "none";
   closemenu.style.display = "block";
 }
 
 function close() {
-  menuNav.style.right = "-100%";
+  nav.style.right = "-100%";
   openmenu.style.display = "block";
   closemenu.style.display = "none";
 }
